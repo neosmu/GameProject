58c2e00ca74594be0011419af9287c643851ab9e
neosmu <hojunglee00@naver.com>
2025-05-28 11:46:51 +0900

    [Feat] Singleton패턴 및 GameManager추가

---
+++ Assets/Scripts/Manager/GameManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : Singleton<GameManager>
+{
+
+    private void Awake() => Init();
+
+    private void Init()
+    {
+        base.SingletonInit();
+    }
+}
