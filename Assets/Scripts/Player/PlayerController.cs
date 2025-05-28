81b93420e6d958d23962f3a5bc9a4b11a003a62e
neosmu <hojunglee00@naver.com>
2025-05-28 14:11:57 +0900

    [Feat] Player 애니메이션 기능 추가

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -5,18 +5,26 @@ using UnityEngine;
 public class PlayerContoller : MonoBehaviour
 {
 
-    [SerializeField] private float moveSpeed; // 움직이는 스피드
+    [SerializeField] private float moveSpeed;
     [SerializeField] private float jumpSpeed;
 
     private Rigidbody2D rigid;
-    private Vector2 inputVec;
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
+
     private float inputX;
     private bool isJumped;
     private bool isGrounded;
 
+    private readonly int IDLE_HASH = Animator.StringToHash("Idle");
+    private readonly int MOVE_HASH = Animator.StringToHash("Move");
+    private readonly int JUMP_HASH = Animator.StringToHash("Jump");
+
     void Start()
     {
         rigid = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     void Update()
@@ -42,12 +50,20 @@ public class PlayerContoller : MonoBehaviour
 
     private void PlayerMove()
     {
+        if (inputX == 0)
+        {
+            animator.Play(IDLE_HASH);
+            return;
+        }
         rigid.velocity = new Vector2(inputX * moveSpeed, rigid.velocity.y);
+        animator.Play(MOVE_HASH);
+        spriteRenderer.flipX = inputX > 0;
     }
 
     private void PlayerJump()
     {
         rigid.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Impulse);
+        animator.Play(JUMP_HASH);
         isJumped = false;
         isGrounded = false;
     }
